7f6b8cee4150b3b1e4d047d02246b2e7de840e3b
dev-liaojianxin <dev-liaojianxin@example.com>
2024-05-09 17:51:02 +0800

    wip(http): 继续回滚

--- vite.config.ts
+++ vite.config.ts
@@ -109,6 +109,14 @@ export default ({ command, mode }) => {
       host: '0.0.0.0',
       hmr: true,
       port: Number.parseInt(VITE_APP_PORT, 10),
+      // 仅 H5 端生效，其他端不生效（其他端走build，不走devServer)
+      // proxy: {
+      //   '/api': {
+      //     target: VITE_SERVER_BASEURL,
+      //     changeOrigin: true,
+      //     rewrite: (path) => path.replace(/^\/api/, ''),
+      //   },
+      // },
     },
     build: {
       // 方便非h5端调试
